4e9d3d52527a0eac6b85a9ebae08aa05a4ba4698
Sophia Nguyen <sophia.nguyen@alumni.ubc.ca>
2019-06-10 01:42:00 -0700

    setting up reducers

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { Provider } from 'react-redux';
 
 import Posts from './components/Posts';
 import PostForm from './components/PostForm';
+import store from './store';
+
 
 
 function App() {
