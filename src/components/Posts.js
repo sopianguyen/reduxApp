2d1421c98d9dc45058e2c48576333289e6a5c1ac
Sophia Nguyen <sophia.nguyen@alumni.ubc.ca>
2019-06-10 08:54:26 -0700

    more progress

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react'
 
 class Posts extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            posts: []
-        }
-    }
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+    //         posts: []
+    //     }
+    // }
 
-    componentWillMount() {
-        fetch('https://jsonplaceholder.typicode.com/posts')
-            .then(res => res.json())
-            .then(data => this.setState({posts: data}));
-    }
+
+
+    // componentWillMount() {
+    //     fetch('https://jsonplaceholder.typicode.com/posts')
+    //         .then(res => res.json())
+    //         .then(data => this.setState({posts: data}));
+    // }
 
     render() {
         const postItems = this.state.posts.map(post => (
